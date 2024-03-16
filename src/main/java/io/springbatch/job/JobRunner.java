2660f766977b2511f6751c7473be14a1d8c94e2b
HYS1753 <hys1753@naver.com>
2024-03-16 23:45:30 +0900

    chore: comment out custom job runner

--- src/main/java/io/springbatch/job/JobRunner.java
+++ src/main/java/io/springbatch/job/JobRunner.java
@@ -1,34 +1,30 @@
 package io.springbatch.job;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
+public class JobRunner {
 
-import java.util.Date;
-
-@RequiredArgsConstructor
-@Component
-public class JobRunner implements ApplicationRunner {
-
-    private final JobLauncher jobLauncher;
-
-    private final Job helloJob;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addString("name", "user1")
-                .addLong("seq", 2L)
-                .addDate("date", new Date())
-                .addDouble("age", 20.0)
-                .toJobParameters();
-
-        jobLauncher.run(helloJob, jobParameters);
-    }
 }
+
+/**
+ * ApplicationRunner를 상속 받아 시스템 자체적으로 run 시키는 방법.
+ */
+
+//@RequiredArgsConstructor
+//@Component
+//public class JobRunner implements ApplicationRunner {
+//
+//    private final JobLauncher jobLauncher;
+//
+//    private final Job helloJob;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addString("name", "user1")
+//                .addLong("seq", 2L)
+//                .addDate("date", new Date())
+//                .addDouble("age", 20.0)
+//                .toJobParameters();
+//
+//        jobLauncher.run(helloJob, jobParameters);
+//    }
+//}
